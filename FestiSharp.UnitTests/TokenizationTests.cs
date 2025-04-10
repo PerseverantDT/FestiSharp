f97bb4db6503438be73964c9c4b073e5d84be6ec
Perseverant Determination <perseverant-determination@outlook.com>
2025-04-11 01:46:02 +0800

    Create initial unit tests

---
+++ FestiSharp.UnitTests/TokenizationTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+using FestiSharp.Tokenization;
+
+namespace FestiSharp.UnitTests;
+
+internal sealed class TokenizationTests
+{
+    [Test]
+    public async Task TestEmptyScriptTokenizationAsync()
+    {
+        var tokenizer = Tokenizer.CreateFromText("");
+
+        await foreach(var token in tokenizer.TokenizeAsync()) {
+            _ = token;
+            Assert.Fail("An empty script should not generate any tokens.");
+        }
+    }
+
+    [Test]
+    public Task TestEmptyScriptTokenization()
+    {
+        var tokenizer = Tokenizer.CreateFromText("");
+
+        foreach (var token in tokenizer.Tokenize()) {
+            _ = token;
+            Assert.Fail("An empty script should not generate any tokens.");
+        }
+
+        return Task.CompletedTask;
+    }
+}
